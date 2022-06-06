40711030fe6baa5c89d588fad9b3e82dbb69986b
Dakotah Ulrath <dulrath@live.com>
2022-06-05 23:17:43 -0400

    syntax fix

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -38,7 +38,7 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `${renderLicenseBadge(data)}
 
-### ${data.title}
+# ${data.title}
 
 ## Description
 ${data.description}
